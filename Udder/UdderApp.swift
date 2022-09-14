f33f418933563e907c553e3755b73b329c736ed0
Jin <m.davit@live.com>
2022-09-14 11:10:40 +0400

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Davit Margaryan on 14.09.22.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
